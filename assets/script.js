c76ba03b3f2e1989a723c6c7f1acc6e8c4e29a44
alylmeier <alylmeier@gmail.com>
2023-01-14 13:55:32 -0500

    more displays populating

--- assets/script.js
+++ assets/script.js
@@ -39,24 +39,31 @@ function getCurrent(e) {
       makeBtn(data);
     });
 }
-
+//this displays the weather now
 function displayCurrent(data) {
+  console.log(data);
   currentWeatherEl.innerHTML = "";
   const nameEl = document.createElement("h2");
   nameEl.textContent = data.name;
-  const tempEl = document.createElement("h5");
-  tempEl.textContent = data.main.temp;
+  const tempEl = document.createElement("h4");
+  tempEl.textContent = data.main.temp + " °F";
 
   currentWeatherEl.appendChild(nameEl);
   currentWeatherEl.appendChild(tempEl);
 }
 
+//this makes the btn for the city entered
 function makeBtn(data) {
   var cityBtn = document.createElement("button");
   cityBtn.textContent = data.name;
   historyEl.appendChild(cityBtn);
 }
+//this isn't working, but you want it to click the button to search
+function useBtn(d) {
+  console.log("ive been clicked");
+}
 
+//this does the second API call to get future data
 function getForecast(data) {
   var lat = data.coord.lat;
   var long = data.coord.lon;
@@ -75,9 +82,24 @@ function displayForecast(data) {
   console.log(data);
   var dayCount = 6;
   for (var i = 1; i < dayCount; i++) {
-    const next5days = document.createElement("h3");
-    next5days.textContent = "Day " + i + ": " + data.list[i].main.temp;
+    //this is also not working; you couldn't get the for loop to pull different increments
+    //const next5daysDate = document.createElement("h3");
+    //next5daysDate.textContent = data.list[i].dt_txt;
+    //fiveDay.appendChild(next5daysDate);
+    const next5days = document.createElement("h5");
+    next5days.textContent = "Day " + i + ": " + data.list[i].main.temp + " °F";
     fiveDay.appendChild(next5days);
+    const next5daysWind = document.createElement("h5");
+    next5daysWind.textContent =
+      "Wind Speed: " + data.list[i].wind.speed + " mph";
+    fiveDay.appendChild(next5daysWind);
+    const next5daysHumid = document.createElement("h5");
+    next5daysHumid.textContent =
+      "Humidity: " + data.list[i].main.humidity + "%";
+    fiveDay.appendChild(next5daysHumid);
+    const next5daysIcon = document.createElement("h5");
+    next5daysIcon.textContent = data.list[i].weather[0].icon + "@2x.png";
+    fiveDay.appendChild(next5daysIcon);
   }
 }
 
@@ -115,3 +137,4 @@ var formSubmitHandler = function (event) {
 // saveTask();
 
 document.querySelector("#enter").addEventListener("click", getCurrent);
+//cityBtn.addEventListener("click", useBtn);
